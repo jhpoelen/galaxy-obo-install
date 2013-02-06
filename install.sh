f7fca437111622cfe0c762423b5f1bd18eb861c6
Jorrit Poelen <jhpoelen@gmail.com>
2013-02-06 10:15:25 -0800

    first version of install scripts

---
+++ install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# steps to get galaxy-obo up and running on aws Ubuntu 12.04
+
+# prereqs
+sudo apt-get install mercurial git
+sudo apt-get install openjdk-6-jre-headless
+sudo apt-get install swi-prolog
+
+# termfinder needs g++ and other build tools
+sudo apt-get install build-essential
+
+# perl dbi / perl DBD::mysql 
+sudo apt-get install libdbi-perl libdbd-mysql-perl
+
+# GO:TermFinder needs dependencies GraphViz and GD graphics libraries 
+sudo apt-get install libgraphviz-perl
+sudo apt-get install libgd-gd2-perl
+
+git clone git://github.com/cmungall/blipkit.git
+# note that: check install by running blipkit/bin/blip produced: ERROR: source_sink `library(odbc)' does not exist
+ 
+git clone http://github.com/cmungall/obo-scripts
+
+# rather than using http://owltools.googlecode.com/files/, chris mentioned to lastest jenkins build instead:
+curl http://build.berkeleybop.org/job/owltools/lastSuccessfulBuild/artifact/OWLTools/OWLTools-Runner/bin/owltools-runner-all.jar >> owltools-runner-all.jar
+
+# install and configure galaxy
+hg clone https://bitbucket.org/cmungall/galaxy-obo
+
+GALAXY_DIR=galaxy-obo
+GALAXY_INI_FILE=GALAXY_DIR/universe_wsgi.ini
+echo host = 0.0.0.0 >> $GALAXY_INI_FILE
+echo tool_config_file = tool_conf_gff.xml,tool_conf_obo.xml >> $GALAXY_INI_FILE
+# point to specific tool config in universe_wsgi.ini
+
+# start galaxy-obo
+sh $GALAXY_DIR/run.sh --reload
+
+# OBO release manager needs java - assuming java 6 is ok
+# curl http://owltools.googlecode.com/files/OBOReleaseManager_unix_0.4.0-20121220.sh > OBOReleaseManager_unix_0.4.0-20121220.sh
+
+# install xserver
+# export DEBIAN_FRONTEND=noninteractive
+# sudo -E apt-get update
+# sudo -E apt-get install -y ubuntu-desktop
+# note that for some reason previous three lines have to be executed to run installer headless (e.g. -c ); this seems counter intuitive to have to install xserver to run something headless. 
+# sh OBOReleaseManager_unix_0.4.0-20121220.sh -c
+
+# listening to Chris, it seemed that blipkit is no longer used. 
+# git clone git://github.com/cmungall/blipkit.git
+# note that: check install by running blipkit/bin/blip produced: ERROR: source_sink `library(odbc)' does not exist
+
+
