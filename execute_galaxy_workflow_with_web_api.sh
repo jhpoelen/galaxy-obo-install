06328cfe4dc7f3585d4316c2640ee9e93d5ba3f6
Jorrit Poelen <jhpoelen@gmail.com>
2013-03-13 16:53:53 -0700

    added some reference to example

--- execute_galaxy_workflow_with_web_api.sh
+++ execute_galaxy_workflow_with_web_api.sh
@@ -1,6 +1,21 @@
+# this example was inspired by 
+#	https://bitbucket.org/galaxy/galaxy-central/src/f1bd817063e0/scripts/api/workflow_execute.py?at=default
+# and
+#	http://wiki.galaxyproject.org/Learn/API#REST
+#
+# The API seems to be documented in python code.
+
 API_KEY=f9fc30fe2bca93c4c6da49163ec33b3d
 GALAXY_API_ENDPOINT=http://galaxy:7474/api/
 
 curl -X POST -H Accept:application/json -H Content-Type:application/json -d '{ "history":"", "workflow_id":"0a248a1f62a0cc04", "ds_map":"" }' -v $GALAXY_API_ENDPOINT/workflows?key=$API_KEY
 
+# this triggered an execution of a input-less workflow and produced result that looks like:
+# {
+#    "history": "0a248a1f62a0cc04", 
+#
+#    "outputs": [
+#        "0a248a1f62a0cc04"
+#    ]
+# Closing connection #0	
 
